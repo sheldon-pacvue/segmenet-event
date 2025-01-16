48eb0ceeb6d43f46da5dc4cb8c1b0ef166213759
Sheldon <sheldon.liu@helium.com>
2025-01-16 13:13:25 +0800

    优化

---
+++ segment-event-service/src/main/java/com/pacvue/segmenteventservice/SegmentEventServiceApplication.java
@@ -0,0 +1,13 @@
+package com.pacvue.segmenteventservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SegmentEventServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SegmentEventServiceApplication.class, args);
+    }
+
+}
---
+++ segment-event-service/src/test/java/com/pacvue/segmenteventservice/SegmentEventServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pacvue.segmenteventservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SegmentEventServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
