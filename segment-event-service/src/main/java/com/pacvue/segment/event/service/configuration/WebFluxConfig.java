3b7a0730935004af004ac3630541c3fe876d7632
Sheldon <sheldon.liu@helium.com>
2025-01-17 09:20:10 +0800

    优化

--- segment-event-service/src/main/java/com/pacvue/segment/event/service/configuration/WebFluxConfig.java
+++ segment-event-service/src/main/java/com/pacvue/segment/event/service/configuration/WebFluxConfig.java
@@ -10,6 +10,6 @@ public class WebFluxConfig implements WebFluxConfigurer {
     @Override
     public void configurePathMatching(PathMatchConfigurer configurer) {
         // 这里设置前缀
-        configurer.addPathPrefix("/segment", c -> true);  // 这会将所有的路径加上 "/api" 前缀
+        configurer.addPathPrefix("/segment", c -> true);  // 这会将所有的路径加上 "/segment" 前缀
     }
 }
